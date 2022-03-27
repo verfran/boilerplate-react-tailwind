8af1287c9e860b5002f7168262d7d74a4a0b895d
Verkeys Francis <verfran@hotmail.com>
2022-03-27 10:59:31 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        tailwind react boilerplate!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
